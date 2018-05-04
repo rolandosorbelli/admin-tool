be35c8f7738a2cf100200203f645288005034a2c
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2018-05-04 15:00:54 +0100

    Created Home and About pages

---
+++ src/components/about/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, eam saperet utroque sensibus te.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Admin Tool</h1>
+        <p>This is an Admin Tool</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
