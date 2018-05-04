8aa3da7c6489bddc8829928f88faf46542c7ac27
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2018-05-04 12:45:52 +0100

    Configured ESLint, npm scripts and testing

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Index.js', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -33,4 +33,4 @@ export default {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }
-}
+};
