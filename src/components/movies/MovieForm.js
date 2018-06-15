7cb7310707f50e6c96c7f59e92e6db561aa91b2b
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2018-06-15 16:59:32 +0100

    Fixed a small bug related to the value of the TextInput of the MovieForm

--- src/components/movies/MovieForm.js
+++ src/components/movies/MovieForm.js
@@ -4,46 +4,46 @@ import TextInput from '../common/TextInput';
 const MovieForm = ({movie, onSave, onChange, loading, errors}) => {
   return (
     <form>
-      <h1>Manage Movie</h1>
+      <h1>Edit Movie</h1>
       <TextInput
         name="title"
         label="Title"
-        vale={movie.title}
+        value={movie.title}
         onChange={onChange}
         error={errors.title}/>
 
       <TextInput
         name="director"
         label="Director"
-        vale={movie.director}
+        value={movie.director}
         onChange={onChange}
         error={errors.director}/>
 
       <TextInput
         name="producer"
         label="Producer"
-        vale={movie.producer}
+        value={movie.producer}
         onChange={onChange}
         error={errors.producer}/>
 
       <TextInput
         name="release_date"
         label="Release Date"
-        vale={movie.release_date}
+        value={movie.release_date}
         onChange={onChange}
         error={errors.release_date}/>
 
       <TextInput
         name="description"
         label="Short Synopsis"
-        vale={movie.description}
+        value={movie.description}
         onChange={onChange}
         error={errors.description}/>
 
       <input
           type="submit"
           disabled={loading}
-          vale={loading ? "Saving..." : "Save"}
+          value={loading ? "Saving..." : "Save"}
           className="btn btn-primary"
           onClick={onSave}/>
     </form>
